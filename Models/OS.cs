28e77eaf1c59ee22181200c9a4e511752e605ede
KellyCarvalho <18kellycarvalho@gmail.com>
2020-07-16 23:19:18 -0300

    models02

--- Models/OS.cs
+++ Models/OS.cs
@@ -7,6 +7,8 @@ namespace projetos.Models
         public int ID { get; set; }
        public Cliente cliente { get; set; }
         public Servico servico { get; set; }
+        public int idServico { get; set; }
+        public int idcliente { get; set; }
         public string Observacoes { get; set; }
 
     }
