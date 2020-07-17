a2dc157288ea9f118c81e68e4a41432bf2f23164
KellyCarvalho <18kellycarvalho@gmail.com>
2020-07-16 23:17:57 -0300

    m02

---
+++ Migrations/20200716222323_atualizandoOS02.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SystemCC.Migrations
+{
+    public partial class atualizandoOS02 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "idServico",
+                table: "os",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "idcliente",
+                table: "os",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "idServico",
+                table: "os");
+
+            migrationBuilder.DropColumn(
+                name: "idcliente",
+                table: "os");
+        }
+    }
+}
